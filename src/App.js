36c7510f65acbf74d3c86523132ab8037d49820b
Henriquelima1 <45356318+Henriquelima1@users.noreply.github.com>
2022-08-02 13:07:47 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <ul>
+        <li>teste</li>
+        <li>henrique</li>
+      </ul>
+    </div>
+  );
+}
