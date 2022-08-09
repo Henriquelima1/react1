a37aab9752e7de581d00a2a3e8e0a79b5631f4de
Henriquelima1 <henriquezardindelima1@gmail.com>
2022-08-09 22:41:55 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import "./styles.css";
 
 import Navbar from "./components/Navbar/Navbar.js";
+import TaskList from "./components/TaskList/TaskList.js";
 
 export default function App() {
   return (
     <div className="App">
       <Navbar />
-      <ul>
-        <li>teste2</li>
-        <li>henrique</li>
-      </ul>
+      <div>
+          <TaskList title="Pendente"/>
+          <TaskList title="Fazendo"/>
+          <TaskList title="Completa"/>
+      </div>
     </div>
   );
 }
